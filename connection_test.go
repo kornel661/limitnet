96097c6136b11457c824f81d64816fdacc6ff468
Kornel Maczyński <kornel661@gmail.com>
2014-08-14 17:08:46 +0100

    tests, the package seems to be working

--- connection_test.go
+++ connection_test.go
@@ -1,7 +1,6 @@
 package limitnet
 
 import (
-	"log"
 	"net"
 	"runtime"
 	"sync"
@@ -35,6 +34,24 @@ func (c *counter) Connect() {
 	}
 }
 
+func (c *counter) All() int {
+	c.Lock()
+	defer c.Unlock()
+	return c.all
+}
+
+func (c *counter) Max() int {
+	c.Lock()
+	defer c.Unlock()
+	return c.max
+}
+
+func (c *counter) Curr() int {
+	c.Lock()
+	defer c.Unlock()
+	return c.curr
+}
+
 func (c *counter) Disconnect() {
 	c.Lock()
 	defer c.Unlock()
@@ -43,17 +60,34 @@ func (c *counter) Disconnect() {
 
 func throttleConn(delay time.Duration, t *testing.T, count *counter) {
 	conn, err := net.Dial("tcp", addr)
-	//log.Println("connected")
-	//defer log.Println("disconnected")
 	if err != nil {
 		t.Fatal(err)
 		return
 	}
-	count.Connect()
-	time.Sleep(delay)
-	count.Disconnect()
+	time.Sleep(delay) // doesn't really matter
 	if err := conn.Close(); err != nil {
 		t.Error(err)
+	}
+}
+
+func server(tl *throttledListener, delay time.Duration, t *testing.T, count *counter) {
+	for {
+		conn, err := tl.Accept()
+		if err != nil {
+			if err.Error() != errClosing.Error() { // closing the listener signals the server to stop
+				t.Fatal(err)
+			}
+			return
+		}
+		// service connection
+		go func() {
+			count.Connect()
+			time.Sleep(delay)
+			count.Disconnect()
+			if err := conn.Close(); err != nil {
+				t.Error(err)
+			}
+		}()
 	}
 }
 
@@ -61,16 +95,28 @@ func TestThrottling(t *testing.T) {
 	max := 10
 	count := &counter{Limit: max, t: t, connected: make(chan struct{}, 2*max+1)}
 	tl := newTL(t)
+	connDelay := 50 * time.Millisecond
+	go server(tl, connDelay, t, count)
 	tl.MaxConns(max)
-	log.Println("Starting connections")
 	for i := 0; i < 10*max; i++ {
-		go throttleConn(10*time.Millisecond, t, count)
+		go throttleConn(5*connDelay, t, count)
 	}
-	log.Println("Waiting for all to connect")
 	for i := 0; i < 10*max; i++ {
 		<-count.connected // wait for all goroutines to connect
 	}
-	log.Println("Closing listener")
 	tl.Close()
+	currClose := count.Curr()
 	tl.Wait()
+	currWait := count.Curr()
+	if currWait > 0 {
+		t.Errorf("Still %d connections active after Wait().", currWait)
+	}
+	t.Logf("Connections active after close: %d, after wait: %d", currClose, currWait)
+	if all := count.All(); all != 10*max {
+		t.Errorf("Only %d connections accepted out of %d", all, 10*max)
+	}
+	t.Logf("Maximum number of concurrent connections: %d, limit: %d", count.Max(), max)
+	if err := tl.Close(); err != errClosing {
+		t.Errorf("Closing the listener twice shoud give %s instead of %s", errClosing, err)
+	}
 }
