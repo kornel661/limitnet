66cdb807e6c791e5f07ab20326397ab9159691e1
Kornel Maczyński <kornel661@gmail.com>
2014-08-14 12:21:12 +0100

    fix package documentation

--- doc.go
+++ doc.go
@@ -1,14 +1,13 @@
 /*
-Package limitnet provides some network primitives, most notably throttled listener, needed by my [nserv](https://github.com/kornel661/nserv) package.
+Package limitnet provides some network primitives, most notably throttled listener, needed by my nserv (https://github.com/kornel661/nserv) package.
 
 Usage:
 
-```go
-import "gopkg.in/kornel661/limitnet.v0"
-```
+	import "gopkg.in/kornel661/limitnet.v0"
+
 or
-```
-go get gopkg.in/kornel661/limitnet.v0
-```
+
+	go get gopkg.in/kornel661/limitnet.v0
+
 */
 package limitnet
