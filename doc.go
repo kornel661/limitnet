14006d46f14e50bc15a366944525a301aafe49eb
Kornel Maczyński <kornel661@gmail.com>
2014-08-19 08:31:15 +0100

    warn about v0 in docs

--- doc.go
+++ doc.go
@@ -10,7 +10,8 @@ or
 
 	go get gopkg.in/kornel661/limitnet.v0
 
-Replace v0 by the version you need.
+Replace v0 by the version you need (v0 is a development version, with no API
+stability guaratees).
 
 */
 package limitnet
